f52e4496c7c0d33036f2030d8100f1db9ff7c091
Channa188 <clea_h@p-hall.net>
2023-04-29 16:11:40 -0700

    Update Score.cs
    added in game score counter

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
    public int playerScore;
+   public TMP_Text playerScoreCount;
 
    private void OnTriggerEnter2D(Collider2D other)
    {
@@ -15,5 +18,6 @@ public class Score : MonoBehaviour
    void AddScore()
    {
       playerScore++;
+      playerScoreCount.text = playerScore.ToString();
    }
 }
