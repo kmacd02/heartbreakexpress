9269c06221a639a366474930a1e5cefce7e3c2fb
Caleb Kim <calebmk21@gmail.com>
2023-04-30 18:57:34 -0700

    ui prototype without scripts

--- Assets/Scripts/UI Scripts/MenuFunctions.cs
+++ Assets/Scripts/UI Scripts/MenuFunctions.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 using System.Diagnostics;
+using Unity.VisualScripting;
+using Debug = System.Diagnostics.Debug;
 
 public class MenuFunctions : MonoBehaviour
 {
-    [Header("How To")]
+    [Header("Tutorial")]
     [SerializeField]
     private VisualTreeAsset HowToMenu;
     private VisualElement Tutorial;
@@ -16,7 +18,11 @@ public class MenuFunctions : MonoBehaviour
     [SerializeField]
     private VisualTreeAsset VolumeMenu;
     private VisualElement VolumeStuff;
-    
+
+    [Header("Letters")] [SerializeField] 
+    private VisualTreeAsset LettersMenu;
+
+    private VisualElement LettersStuff;
     
     
     // Initializing Main Menu Buttons
@@ -26,11 +32,35 @@ public class MenuFunctions : MonoBehaviour
     public Button QuitButton;
     public Button MenuButton;
     public Button HowToButton;
+    public Button LettersButton;
+    
+    private VisualElement Container;
+    private VisualElement LettersPanel;
+    //private VisualElement VolumePanel;
     
         // Start is called before the first frame update
     void Start()
     {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        Container = root.Q<VisualElement>("Container");
+        //HowToWrapper = root.Q<VisualElement>("HowToWrapper");
         
+
+        // Gathering Button Roots
+
+        PlayButton = root.Q<Button>("PlayButton");
+        OptionsButton = root.Q<Button>("OptionsButton");
+        QuitButton = root.Q<Button>("QuitButton");
+        MenuButton = root.Q<Button>("MenuButton");
+        HowToButton = root.Q<Button>("HowToButton");
+        LettersButton = root.Q<Button>("LettersButton");
+        
+        PlayButton.clicked += PlayGame;
+        OptionsButton.clicked += OptionsMenu;
+        //QuitButton.clicked += ExitGame;
+        //BackButton.clicked += BackButtonClicked;
+        //HowToButton.clicked += DisplayTutorial;
+        //BackVolume.clicked += BackButtonClicked;
     }
 
     // Update is called once per frame
@@ -38,4 +68,48 @@ public class MenuFunctions : MonoBehaviour
     {
         
     }
+    
+    void PlayGame()
+    {
+        //SFX_Source.Play();
+        //SceneManager.LoadScene(SCENE NAME);
+        UnityEngine.Debug.Log("we get to play the game lmao");
+    }
+
+    void OptionsMenu()
+    {
+        UnityEngine.Debug.Log("Options Opened");
+    }
+    
+    /*void DisplayTutorial()
+    {
+        //SFX_Source.Play();
+        Container.Clear();
+        //Container.Add(Tutorial);
+    }
+
+    void BackButtonClicked()
+    {
+        SFX_Source.Play();
+        Container.Clear();
+        Container.Add(PlayButton);
+        Container.Add(OptionsButton);
+        Container.Add(HowToButton);
+        Container.Add(QuitButton);
+    }
+
+    void VolumeSettings()
+    {
+        SFX_Source.Play();
+        Container.Clear();
+        Container.Add(VolumeStuff);
+
+    }
+
+    void ExitGame()
+    {
+        SFX_Source.Play();
+        Application.Quit();
+        UnityEngine.Debug.Log("Quit.");
+    }*/
 }
